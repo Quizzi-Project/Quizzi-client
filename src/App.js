aebad78575f8a08d5616578a158c0d9ed3db0bc9
enbarSoco <enbar2232@gmail.com>
2022-02-02 11:45:51 +0200

    init files

---
+++ src/App.js
@@ -0,0 +1,6 @@
+const app = () => {
+  return (
+    <h1>hello world</h1>);
+}
+
+export default app;
---
+++ src/Router/router.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+
+const ReactRouter = () => {
+    return(
+        <Routes>
+            {/* <Route exact path="/" element={<IdeasList/>}/> */}
+        </Routes>
+    )
+}
+export default ReactRouter;
