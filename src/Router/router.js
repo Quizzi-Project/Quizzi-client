067976c9acfdecae47e4a6581b34bcc23bc33fa0
enbarSoco <enbar2232@gmail.com>
2022-02-04 11:20:13 +0200

    added empty homepage

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from '../Pages/Dashboard';
+import Home from '../Pages/Home';
 import Login from '../Pages/Login';
 import Register from '../Pages/Register';
 
 const ReactRouter = () => {
     return (
         <Routes>
+            <Route path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/register" element={<Register />} />
             <Route exact path="/dashboard" element={<Dashboard />} />
