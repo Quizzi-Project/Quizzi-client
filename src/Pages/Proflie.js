2977289572e6a84132b3c1dd403913c9d9c04686
enbarSoco <enbar2232@gmail.com>
2022-02-13 14:11:25 +0200

    bug fixed

---
+++ src/Pages/Proflie.js
@@ -0,0 +1,12 @@
+import Header from "../Components/Header/Header";
+const Profile = () => {
+    return (
+        <>
+            <Header />
+            <h1>Profile</h1>
+        </>
+
+    );
+}
+
+export default Profile;
